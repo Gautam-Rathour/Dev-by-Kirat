51456466ba0222fe4de79aa9d4a5c0935095b427
Gautam-Rathour <gau321tam@gmail.com>
2025-04-01 13:53:15 +0530

    Add Code

---
+++ Cohort-3/Week-4/harkirat-class-node/index.js
@@ -0,0 +1,64 @@
+
+// const chalk = require("chalk");
+
+// console.log(chalk);
+
+
+// function sum (a , b) {
+//     return a + b;
+// }
+
+// function mul(a , b) {
+//     return a * b;
+// }
+
+// console.log(sum(5, 4));
+
+
+
+
+
+
+const path = require("path");
+console.log(__dirname);
+console.log(path.join(__dirname, "index.js"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
