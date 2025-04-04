521d497475f3a13723e62c2a89850ea19b8a0669
Gautam-Rathour <gau321tam@gmail.com>
2025-04-04 08:50:18 +0530

    Add

--- Cohort-3/Week-4/Week-4-Offline/Postman/index.js
+++ Cohort-3/Week-4/Week-4-Offline/Postman/index.js
@@ -121,12 +121,18 @@
 
 
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 
 app.get("/files/:fileName", function(req, res) {
     const name = req.params.fileName;
     console.log(name);
-    res.json({});
+    fs.readFile(name, "utf-8", function(err, data) {
+        res.json({
+            data
+        })
+    })
 })
 
 app.listen(3000);  // server is running on port 3000
