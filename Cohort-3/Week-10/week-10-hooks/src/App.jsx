514713b1635d3633b93ed12647bf1f2bd3934490
Gautam-Rathour <gau321tam@gmail.com>
2025-05-16 18:08:43 +0530

    Add Reach file with Code

---
+++ Cohort-3/Week-10/week-10-hooks/src/App.jsx
@@ -0,0 +1,68 @@
+
+
+
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+
+
+function App() {
+  
+  return <div>
+
+      <BrowserRouter>
+          
+
+
+      <Routes>
+        <Route path="/" element={< Layout />} >
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program/>} />
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program/>} />
+          <Route path="/" element={< Landing />} />
+          <Route path="*" element={<ErrorPage/>} />
+         </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+}
+
+function Layout () {
+  return <div style={{height: "100vh"}}>
+      <Link to="/">Allen</Link> |
+      <Link to="/neet/online-coaching-class-11">Class-11</Link> |
+      <Link to="/neet/online-coaching-class-12">Class-12</Link> | 
+      
+      <div style={{height: "90vh"}}>
+        <Outlet/> 
+      </div>
+    
+    Footer
+  </div>
+}
+function ErrorPage () {
+  return <div>
+    Sorry page not found
+  </div>
+}
+function Landing() {
+  return <div>
+    welcome to allen
+  </div>
+}
+
+function Class11Program() {
+  return <div>
+    NEET programs for Class 11th
+  </div>
+}
+
+function Class12Program() {
+  const navigate = useNavigate();
+
+  function redirectUser() {
+    navigate("/");
+  }
+  return <div>
+    NEET programs for Class 12th
+    <button onClick={redirectUser}>Go back to the landing page</button>
+  </div>
+}
+export default App
