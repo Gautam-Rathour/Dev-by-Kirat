b8e9cf603f440b41fdbb2610160dfe03c6776317
Gautam-Rathour <gau321tam@gmail.com>
2025-06-19 18:21:29 +0530

    Add Custom Hooks Code

---
+++ Cohort-3/Week-11/week-11-react-1/src/App.jsx
@@ -0,0 +1,83 @@
+
+
+// import { useState } from 'react'
+// import './App.css'
+
+// // custom hook
+// function useCounter() {
+//   const [count, setCount] = useState(0);
+
+//   function increaseCount() {
+//     setCount(count + 1);
+//     // setCount(c => c + 1);
+//   }
+//   return {
+//     count: count,
+//     increaseCount: increaseCount
+//   }
+// }
+
+// function App() {
+
+//   return (
+//     <div>
+//       <Counter/>
+//       <Counter/>
+//       <Counter/>
+//     </div>
+//   )
+// }
+
+
+// function Counter() {
+// const {count, increaseCount} = useCounter();
+
+//  return (
+//     <div>
+//       <button onClick={increaseCount}>Increase {count} </button>
+//     </div>
+//   )
+// }
+
+
+
+// export default App
+
+
+
+// =====================================================================================
+// =====================================================================================
+// =====================================================================================
+// =====================================================================================
+
+
+
+
+
+import { useState, useEffect } from 'react'
+import './App.css'
+import { useFetch } from './hooks/useFetch'
+
+function App() {
+  const [currentPost, setCurrentPost] = useState(1);
+  const { finalData, loading, error } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost, 10 );
+
+  if(loading) {
+    return <div>
+      Lopading...
+    </div>
+  }
+
+  return (
+    <div>
+      <button onClick={() => setCurrentPost(1)}>1</button>
+      <button onClick={() => setCurrentPost(2)}>2</button>
+      <button onClick={() => setCurrentPost(3)}>3</button>
+      {JSON.stringify(finalData)}
+    </div>
+  )
+}
+
+
+export default App
+
