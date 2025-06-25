cfbb6deb8b31de6c8e79d69b9136d0787678e422
Gautam-Rathour <gau321tam@gmail.com>
2025-06-25 21:18:38 +0530

    Add Week-13 Folder and file

---
+++ Cohort-3/Week-13/week-13.1-tailwind-class/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <div style={{display: "flex", justifyContent: "center"}}> */}
+      <div className="flex justify-between">
+        <div>
+          child 1
+        </div>
+        <div>
+          child 2
+        </div>
+        <div>
+          child 3
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
