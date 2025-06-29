60cf753a8dbb5b45d796d0160271c5363e5f081f
Gautam-Rathour <gau321tam@gmail.com>
2025-06-29 12:12:38 +0530

    Add week-13.1 Code

--- Cohort-3/Week-13/week-13.1-tailwind-class/src/components/Otp.jsx
+++ Cohort-3/Week-13/week-13.1-tailwind-class/src/components/Otp.jsx
@@ -3,36 +3,28 @@ import Button from './Buttons'
 import { useRef, useState } from 'react';
 
 
-function Otp() {
-    const ref1 = useRef();
-    const ref2 = useRef();
-    const ref3 = useRef();
-    const ref4 = useRef();
-    const ref5 = useRef();
-    const ref6 = useRef();
+// 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+function Otp({ number }) {
+    const ref = useRef(Array(number).fill(0));
 
     const [disabled, setDisabled] = useState(true);
 
     return <div className="flex justify-center">
-        <SubOtpBox ref={ref1} onDone={() => {
-            ref2.current.focus();
-        }}/>
-        <SubOtpBox ref={ref2} onDone={() => {
-            ref3.current.focus();
-        }}/>
-        <SubOtpBox ref={ref3} onDone={() => {
-            ref4.current.focus();
-        }}/>
-        <SubOtpBox ref={ref4} onDone={() => {
-            ref5.current.focus();
-        }}/>
-        <SubOtpBox ref={ref5} onDone={() => {
-            ref6.current.focus();
-        }}/>
-        <SubOtpBox ref={ref6} onDone={() => {
-            setDisabled(false);
-        }}/>
-
+       
+    {number} 
+    {Array(number).fill(1).map((x, index) => <SubOtpBox reference={e => ref.current[index] = e} key={index} onDone={() => {
+        console.log(ref)
+        console.log(index)
+        if(index + 1 >= number) {
+            return
+        }
+        ref.current[index + 1].focus();
+    }} goBack={() => {
+        if(index == 0) {
+            return
+        }
+        ref.current[index - 1].focus();
+    }} />)}
 
         <br/>
         <Button disabled={disabled}>Sign up</Button>
@@ -40,12 +32,27 @@ function Otp() {
 }
 
 function SubOtpBox({
-    ref, onDone
+    reference, onDone, goBack
 }) {
+    const [inputBoxVal, setInputBoxVal] = useState("");
+
     return <div>
-        <input ref={ref} onChange={(e) => {
-            onDone()
-        }} type="text" className="m-1 w-[40px] h-[50px] rounded-2xl bg-blue-500 outline-none px-4 text-white"></input>
+        <input value={inputBoxVal} ref={reference} onKeyUp={(e) => {
+            if(e.key == "Backspace") {
+                goBack();
+            }
+        }} onChange={(e) => {
+            const val = e .target.value;
+
+
+            if(val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9" || val == "0") {
+                setInputBoxVal(val);
+                onDone() 
+            } else {
+
+            }
+            
+        }} type="text" className="m-1 w-[40px] h-[50px] rounded-xl bg-blue-500 outline-none px-4 text-white"></input>
     </div>
 }
 
