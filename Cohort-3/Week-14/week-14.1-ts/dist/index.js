a475639b76499ebfc054bb9565d48658d044133d
Gautam-Rathour <gau321tam@gmail.com>
2025-07-01 19:22:42 +0530

    Add week-14.2 interface AND type Code

---
+++ Cohort-3/Week-14/week-14.1-ts/dist/index.js
@@ -0,0 +1,52 @@
+"use strict";
+// let x: number | string = 10;
+// x = "harkirat Singh"
+// console.log(x);
+// ==============================================================
+// let yourName: string = "Harkirat Singh";
+// console.log("Hello, " + yourName );
+// ==============================================================
+// function greet(name: String) {
+//     console.log("hello, " + name);
+// }
+// greet("Monu");
+// ==============================================================
+// function sum(a: number, b: number) {
+//     return a + b;
+// }
+// let ans = sum(4, 8);
+// console.log(ans);
+// ==============================================================
+// function delayCall(fn: () => void) {
+//     setTimeout(fn, 2000);
+// }
+// delayCall(function() {
+//     console.log("hi there");
+// })
+// ==============================================================
+// function delayCall(anotherFn: () => void) {
+//     setTimeout(anotherFn, 2000);
+// }
+// function log() {
+//     console.log("hi there");
+// }
+// delayCall(log);
+// ==============================================================
+// let greet = (name) => {
+//     console.log("hi there");
+// }
+// ==============================================================
+// ==============================================================
+// ==============================================================
+function greet(user) {
+    console.log("hello, " + user.name);
+}
+// greet({
+//     name: "Kirat",
+//     age: 21,
+// })
+let user = {
+    name: "kirat",
+    age: 21,
+};
+greet(user);
