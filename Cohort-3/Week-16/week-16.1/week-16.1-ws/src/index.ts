b6957f317e4fcc7d89b5714775dafb37df1504c6
Gautam-Rathour <gau321tam@gmail.com>
2025-07-22 22:45:48 +0530

    Add week-16.1 code

---
+++ Cohort-3/Week-16/week-16.1/week-16.1-ws/src/index.ts
@@ -0,0 +1,23 @@
+
+
+
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080});
+
+
+// event handler
+wss.on("connection", function(socket) {
+    console.log("user connected")
+
+
+    socket.on("message", (e) => {
+        console.log(e.toString());
+        console.log(e.toString() === "ping");
+
+        if(e.toString() === "ping") {
+            socket.send("pong");
+        };
+    })
+})
+
