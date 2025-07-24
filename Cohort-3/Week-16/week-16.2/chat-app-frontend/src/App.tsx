afcd9f5a35db3a0368f48c30a7738d134d7a7efb
Gautam-Rathour <gau321tam@gmail.com>
2025-07-24 16:41:16 +0530

    Add frontend chat-app Code

--- Cohort-3/Week-16/week-16.2/chat-app-frontend/src/App.tsx
+++ Cohort-3/Week-16/week-16.2/chat-app-frontend/src/App.tsx
@@ -1,29 +1,67 @@
 
-import './App.css'
-import { useEffect, useRef } from "react";
+import "./App.css"
+import { useState, useEffect, useRef } from "react";
+
+
 
 function App() {
-  const inputRef = useRef();
-  
-
-  function sendMessage() {
-    const inputmsg = inputRef.current.value;
-    socket.send(inputmsg);
-  }
+  const [messages, setMessages] = useState(["hi there..", "hello.."]);
+  const wsRef = useRef();
 
   useEffect(() => {
-    const ws = new WebSocket("ws://loclhost:8080");
+    const ws = new WebSocket("http://localhost:8080");
 
-    
-  })
- 
+    ws.onmessage = (event) => {
+      setMessages(m => [...m, event.data])
+    }
+    wsRef.current = ws;
 
-  return (
-    <>
-      <input ref={inputRef} type="text" placeholder="Send message. .."  />
-      <button type="submit" onClick={sendMessage} >Submit</button>
-    </>
-  )
+
+    // when anyone connect => he will connnect to red room!
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        type: "join",
+        payload: {
+          roomId: "red"
+        }
+      }))
+    }
+
+      // cleanUp for useEffect
+      return () => {
+        ws.close()
+      }
+  }, []);
+
+
+    return (
+      <div className="h-screen bg-black">
+        <br/> <br/> <br/>
+          <div className="h-[80vh]">
+            {messages.map(message => <div className="m-8">
+              <span className="bg-white text-black rounded p-4 m-8">
+                  {message} 
+              </span>
+            </div>)}
+          </div>
+          <div className="w-full bg-white flex">
+            <input id="message" className="flex-1 p-4"></input>
+            <button onClick={() => {
+              const message = document.getElementById("message")?.value;
+              wsRef.current.send(JSON.stringify({
+                type: "chat",
+                payload: {
+                  message: message
+                }
+              }))
+
+            }} className="bg-purple-600 text-white p-4">
+              Send message
+            </button>
+
+          </div>
+      </div>
+    )
 }
 
 export default App
