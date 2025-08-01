22f59981114c94db3319a143d2af6bedfecfd4d3
Gautam-Rathour <gau321tam@gmail.com>
2025-08-01 16:41:59 +0530

    Add how to prevent from SQL enjection

--- Cohort-3/Week-17/week-17-postgres/src/index.ts
+++ Cohort-3/Week-17/week-17-postgres/src/index.ts
@@ -13,24 +13,6 @@ const pgClient = new Client('postgresql://neondb_owner:npg_8TcI2kxpzOCd@ep-curly
 pgClient.connect();
 
 
-// const pgClient = new Client({
-//     user: "neondb_owner",
-//     password: "npg_8TcI2kxpzOCd",
-//     port: 5432,
-//     host: "ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech",
-//     database: "neondb",
-//     ssl: true
-// })
-
-
-// async function main() {
-//     const response = await pgClient.query("SELECT * FROM users");
-//     console.log(response);
-// }
-
-// main();
-
-
 
 app.post("/signup", async (req, res) => {
     const username = req.body.username;
