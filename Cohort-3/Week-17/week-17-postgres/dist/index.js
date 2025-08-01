acb464a9b3d30898fe9a3cbcf8b183c6d8cd3fef
Gautam-Rathour <gau321tam@gmail.com>
2025-08-01 16:26:39 +0530

    Add postgres Code

--- Cohort-3/Week-17/week-17-postgres/dist/index.js
+++ Cohort-3/Week-17/week-17-postgres/dist/index.js
@@ -8,22 +8,45 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const pg_1 = require("pg");
-// const pgClient = new Client('postgresql://neondb_owner:npg_8TcI2kxpzOCd@ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
-const pgClient = new pg_1.Client({
-    user: "neondb_owner",
-    password: "npg_8TcI2kxpzOCd",
-    port: 5432,
-    host: "ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech",
-    database: "neondb",
-    ssl: true
-});
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield pgClient.connect();
-        const response = yield pgClient.query("USERS * FROM users");
-        console.log(response);
-    });
-}
-main();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const pgClient = new pg_1.Client('postgresql://neondb_owner:npg_8TcI2kxpzOCd@ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+pgClient.connect();
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_8TcI2kxpzOCd",
+//     port: 5432,
+//     host: "ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech",
+//     database: "neondb",
+//     ssl: true
+// })
+// async function main() {
+//     const response = await pgClient.query("SELECT * FROM users");
+//     console.log(response);
+// }
+// main();
+app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`;
+        const response = yield pgClient.query(insertQuery, [username, email, password]);
+        res.json({
+            message: "You have signed up"
+        });
+    }
+    catch (e) {
+        console.log(e);
+        res.json({
+            message: "Error while signing up"
+        });
+    }
+}));
+app.listen(3000);
--- Cohort-3/Week-17/week-17-postgres/src/index.ts
+++ Cohort-3/Week-17/week-17-postgres/src/index.ts
@@ -1,26 +1,61 @@
 
+
+
+import express from "express";
 import { Client } from "pg";
 
-// const pgClient = new Client('postgresql://neondb_owner:npg_8TcI2kxpzOCd@ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+
+const app = express();
+app.use(express.json());
 
 
-const pgClient = new Client({
-    user: "neondb_owner",
-    password: "npg_8TcI2kxpzOCd",
-    port: 5432,
-    host: "ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech",
-    database: "neondb",
-    ssl: true
+const pgClient = new Client('postgresql://neondb_owner:npg_8TcI2kxpzOCd@ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+pgClient.connect();
+
+
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_8TcI2kxpzOCd",
+//     port: 5432,
+//     host: "ep-curly-forest-ad3h77on-pooler.c-2.us-east-1.aws.neon.tech",
+//     database: "neondb",
+//     ssl: true
+// })
+
+
+// async function main() {
+//     const response = await pgClient.query("SELECT * FROM users");
+//     console.log(response);
+// }
+
+// main();
+
+
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+
+    try {
+
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`
+
+        const response = await pgClient.query(insertQuery, [username, email, password]);
+
+        res.json({
+            message: "You have signed up"
+        })
+    } catch (e) {
+        console.log(e)
+        res.json ({
+            message: "Error while signing up"
+        })
+    }
+    
 })
 
 
-async function main() {
-    await pgClient.connect();
-    const response = await pgClient.query("USERS * FROM users");
-    console.log(response);
-}
-
-
-main();
-
+app.listen(3000);
 
