9a431400a1fc582bb754995e98833493853fa1d8
Gautam-Rathour <gau321tam@gmail.com>
2025-08-04 15:25:52 +0530

    Add Error code for take help by chatGPT

--- Cohort-3/Week-18/week-18.1-prisma/src/index.ts
+++ Cohort-3/Week-18/week-18.1-prisma/src/index.ts
@@ -1,17 +1,54 @@
 
 
+
+
+import express from "express";
 import { PrismaClient } from '@prisma/client'
 
+
+const app = express();
 const client = new PrismaClient();
 
 
 
-async function createUser() {
+app.get("/users", async (req, res) => {
+    const users = await client.user.findMany();
+    res.json({
+        users
+    })
+})
+
+app.get("/todos/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const user = await client.user.findFirst({
+        where: {
+            id: parseInt(id)
+        }, 
+        select: {
+            todos: true,
+            username: true,
+            password: true
+        }
+    });
+    res.json({
+        user
+    })
+})
+
+
+app.listen(3000);
+
+
+
+
+
+
+async function getUser() {
     const user = await client.user.findFirst({
         where: {
             id: 1
-        },
-        include: {
+        }, include: {
             todos: true
         }
     })
@@ -19,9 +56,9 @@ async function createUser() {
     console.log(user)
 }
 
+getUser();
 
 
-createUser();
 
 
 
