785f4cd3ea9b28bfea233ae61915276adb077a63
Gautam-Rathour <gau321tam@gmail.com>
2025-08-08 21:44:52 +0530

    Add old code

--- Cohort-3/Week-19/week-19.1-next-app/app/user/page.tsx
+++ Cohort-3/Week-19/week-19.1-next-app/app/user/page.tsx
@@ -10,6 +10,9 @@ import axios from "axios";
 export default async function User() {
 
     const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+
+    await new Promise(r => setTimeout(r, 5000));
+    
     const data = response.data;
     console.log("hi");
 
